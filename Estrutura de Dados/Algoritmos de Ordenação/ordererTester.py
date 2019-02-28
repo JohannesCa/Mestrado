562cfccdcdf70b35047ce3e289b4e6a7cc4be210
johannesca <johannes.cavalcante@oulook.com>
2019-02-28 00:08:25 -0300

    Adicionada função para leitura de arquivos e edição no prompt para aceitar valores quaisquer nos testes.

--- Estrutura de Dados/Algoritmos de Ordenação/ordererTester.py
+++ Estrutura de Dados/Algoritmos de Ordenação/ordererTester.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+import re
+import sys
+import argparse
 import numpy as np
 import inquirer as inq
 from pprint import pprint as prt
@@ -13,17 +16,17 @@ questions = [
              message="Qual algoritmo de ordenação deseja testar?",
              choices=["Insertion Sort", "Selection Sort"]
              ),
-    inq.List('arraySize',
-             message="Qual o tamanho do array?",
-             choices=[10, 30, 50, 100, 1000, 10000]
+    inq.Text('arraySize',
+             message="Qual o tamanho para o array?",
+             validate=lambda _, x: re.match('^[0-9]*[0-9]$', x)
              ),
-    inq.List('arrayMax',
-             message="Qual o valor máximo para os números do array?",
-             choices=[10, 50, 100, 1000]
+    inq.Text('maxValue',
+             message="Qual o MAIOR valor possível para os números do array?",
+             validate=lambda _, x: re.match('^[0-9]*[0-9]$', x)
              ),
     inq.List('numTests',
              message="Quantos testes fazer?",
-             choices=[1, 10, 100]
+             choices=[1, 10, 100, 1000]
              ),
     inq.List('verbose',
              message="Verbose?",
@@ -32,10 +35,16 @@ questions = [
 ]
 
 
-def run(options):
+def fileReader(filename):
+    with open(filename, 'r') as file:
+        lines = [int(x.split('\n')[0]) for x in list(file.readlines())]
+        return lines
+
+
+def test(options):
     for i in range(options['numTests']):
         testArray = np.random.randint(
-            options['arrayMax'], size=options['arraySize'])
+            int(options['maxValue']), size=int(options['arraySize']))
 
         if options['sorter'] == "Insertion Sort":
             mySorter = InsertionSorter(testArray)
@@ -66,5 +75,51 @@ def run(options):
 
 
 if __name__ == "__main__":
-    options = inq.prompt(questions)
-    run(options)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--selection', help='Use selection sorter.', action='store_true')
+    parser.add_argument(
+        '--insertion', help='Use insertion sorter.', action='store_true')
+    parser.add_argument(
+        '-f', '--file', help='Select a file with a list of numbers.')
+
+    args = parser.parse_args()
+
+    if args.file:
+        testArray = fileReader(args.file)
+
+        if args.selection:
+            print(' --- Running Selecion Sort')
+            mySorter = SelectionSorter(testArray)
+            res = mySorter.sort()
+            ref = np.sort(testArray)
+
+            if np.array_equal(res, ref):
+                print(' --- Selection Sort is Correct!')
+                print(' +++ Sorted Array:')
+                prt(res)
+                print('')
+            else:
+                print(' --- Selection Sort is WRONG!')
+
+        if args.insertion:
+            print(' --- Running Insertion Sort')
+            mySorter = InsertionSorter(testArray)
+            res = mySorter.sort()
+            ref = np.sort(testArray)
+
+            if np.array_equal(res, ref):
+                print(' --- Insertion Sort is Correct!')
+                print(' +++ Sorted Array:')
+                prt(res)
+                print('')
+            else:
+                print(' --- Insertion Sort is WRONG!')
+
+    elif not (args.selection or args.insertion):
+        options = inq.prompt(questions)
+        test(options)
+
+    else:
+        parser.print_help(sys.stderr)
+        sys.exit(-1)
